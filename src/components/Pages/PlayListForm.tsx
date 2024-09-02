17026daa01d242e7b45cf6eef79c28b7466c699f
Marco Frencia <marco.frencia@gmail.com>
2024-09-02 18:09:36 -0300

    vista previa a la derecha

--- src/components/Pages/PlayListForm.tsx
+++ src/components/Pages/PlayListForm.tsx
@@ -1,8 +1,14 @@
 import React, { useState } from "react";
 import styles from "./PlayListForm.module.css";
+import AlbumCard from "../Cards/AlbumCard";
 
 interface PlaylistFormProps {
-  onSubmit: (title: string, description: string, imageUrl: string, genre: string) => void;
+  onSubmit: (
+    title: string,
+    description: string,
+    imageUrl: string,
+    genre: string
+  ) => void;
   onCancel: () => void;
 }
 
@@ -18,66 +24,69 @@ const PlaylistForm: React.FC<PlaylistFormProps> = ({ onSubmit, onCancel }) => {
 
   return (
     <section id="formulario" className={styles.formulario_container}>
-      <h2 className={styles.formulario_tituloh2}>Crear Nueva Playlist</h2>
-      <label htmlFor="title" >
-        Título:
-      </label>
-      <input className={styles.formulario_input}
-        id="title"
-        value={title}
-        onChange={(e) => setTitle(e.target.value)}
-      />
-      <label htmlFor="description" className="form-content">
-        Descripción:
-      </label>
-      <input className={styles.formulario_input}
-        id="description"
-        value={description}
-        onChange={(e) => setDescription(e.target.value)}
-      />
-      <label htmlFor="Imagen" className="form-content">
-        ImageUrl:
-      </label>
-      <input className={styles.formulario_input}
-        id="Imagen"
-        value={imageUrl}
-        onChange={(e) => setImageUrl(e.target.value)}
-      />
-    <label htmlFor="genre" className="form-content">
-        Privacidad:
-      </label>
-      <select
-        className={styles.formulario_input}
-        id="genre"
-        value={priv}
-        onChange={(e) => setPriv(e.target.value)}
-      >
-        
-        <option value="publica">Pública</option>
-        <option value="privada">Privada</option>
-        <option value="oculta">Oculta</option>        
-      </select>
+  <div className={styles.form_section}> 
+    {/* Añadí esta nueva clase para agrupar los inputs y evitar que interfieran con la vista previa */}
+    
+    <h2 className={styles.formulario_tituloh2}>Crear Nueva Playlist</h2>
+    <label htmlFor="title">Título:</label>
+    <input
+      className={styles.formulario_input}
+      id="title"
+      value={title}
+      onChange={(e) => setTitle(e.target.value)}
+    />
+    <label htmlFor="description" className="form-content">
+      Descripción:
+    </label>
+    <input
+      className={styles.formulario_input}
+      id="description"
+      value={description}
+      onChange={(e) => setDescription(e.target.value)}
+    />
+    <label htmlFor="Imagen" className="form-content">
+      ImageUrl:
+    </label>
+    <input
+      className={styles.formulario_input}
+      id="Imagen"
+      value={imageUrl}
+      onChange={(e) => setImageUrl(e.target.value)}
+    />
+    <label htmlFor="privacy" className="form-content">
+      Privacidad:
+    </label>
+    <select
+      className={styles.formulario_input}
+      id="privacy"
+      value={priv}
+      onChange={(e) => setPriv(e.target.value)}
+    >
+      <option value="publica">Pública</option>
+      <option value="privada">Privada</option>
+      <option value="oculta">Oculta</option>
+    </select>
 
-
-
-      <div className={styles.button_container}>
-        <button className={styles.formulario_boton} onClick={handleSubmit}>
+    <div className={styles.button_container}>
+      <button className={styles.formulario_boton} onClick={handleSubmit}>
         Crear
-      </button>      
-      
+      </button>
       <button className={styles.formulario_boton} onClick={onCancel}>
         Cancelar
       </button>
-      </div>
-      <div className={styles.preview_container}>
-        <h3>Vista Previa:</h3>
-        <p><strong>Título:</strong> {title}</p>
-        <p><strong>Descripción:</strong> {description}</p>
-        <p><strong>URL de Imagen:</strong> {imageUrl}</p>
-        <p><strong>Privacidad:</strong> {priv}</p>
-      </div>
+    </div>
+  </div>
 
-    </section>
+  <div className={styles.preview_container}>
+    <AlbumCard
+      id={1}
+      title={description}
+      artist={title}
+      imageUrl={imageUrl}
+      name={title}
+    />
+  </div>
+</section>
   );
 };
 
